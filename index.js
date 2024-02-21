a413bc36a37d4b78df61fe53252ef1fdc0875741
Suvra Ghosh <ghoshsuvra2566@gmail.com>
2024-02-21 23:17:25 +0530

    Updated Portfolio

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-
-document.querySelector('.read-more-button').addEventListener('click', function () {
-    const content = document.querySelector('.content');
-    const readMoreButton = document.querySelector('.read-more-button');
-    const readMoreSection = document.querySelector('.read-more');
-
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-        readMoreButton.textContent = 'Read More';
-        readMoreSection.classList.remove('expanded');
-    } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-        readMoreButton.textContent = 'Read Less';
-        readMoreSection.classList.add('expanded');
-    }
+var typed = new Typed('#element', {
+    strings: ['Suvra', 
+              'Frontend Devloper',
+              'Software Developer',
+              'Full-Stack Developer',],
+    typeSpeed: 80,
+    bindInputFocusEvents: true,
 });
+
 
